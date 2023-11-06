54052c8b53e1c141ac4876e3911093c414255f11
Kenni Kollemorten <kenn7575@gmail.com>
2023-11-06 13:33:19 +0100

    add folder

---
+++ src/compenents/LoginForm.svelte
@@ -0,0 +1,20 @@
+<script>
+    import {LogIn,} from '../functions/firebase'
+   
+</script>
+
+<div class="card">
+
+   
+</div>
+<div class="card w-96 bg-neutral text-neutral-content">
+    <div class="card-body items-center text-center">
+      <h2 class="card-title">Cookies!</h2>
+      <p>We are using cookies for no reason.</p>
+
+        <button class="btn btn-primary" on:click={LogIn}>
+            login with google 
+        </button>
+
+    </div>
+  </div>
