433d97603fb873575cdbd4de9af41a110eee97a5
Kenni Kollemorten <kenn7575@gmail.com>
2023-11-06 14:49:45 +0100

    work

--- src/App.svelte
+++ src/App.svelte
@@ -2,14 +2,20 @@
   import './app.css'
   import {LogOut} from './functions/firebase'
   import Restricted from './components/restricted.svelte';
+  import { isDoneLoadingStore } from "./stores/loadingStore";
+
   
 </script>
 
 <main class="flex min-h-screen justify-center items-center">
+  {#if $isDoneLoadingStore}
   <Restricted>
     <!-- app -->
     <button on:click={LogOut}>
       sign out
     </button>
   </Restricted>
+  {:else}
+    <span class="loading loading-spinner loading-lg"></span>
+  {/if}
 </main>
