d73957c824153b5d9212c89b935add9984f65cf3
Kenni Kollemorten <kenn7575@gmail.com>
2023-11-06 21:32:44 +0100

    work on sortable drag and drop

--- src/pages/Home.svelte
+++ src/pages/Home.svelte
@@ -2,7 +2,7 @@
   import Sidebar from '../components/Sidebar.svelte'
   import {userData} from '../stores/userDataStore'
   import { projectStore } from '../stores/projectStore'
-
+import DragList from '../components/DragList.svelte';
   import { onMount } from 'svelte';
 
   let projectId;
@@ -23,6 +23,30 @@
     <div class="flex-grow pl-[18rem]">
       <div>
         <h1>Project id: { $projectStore.uid }</h1>
+        <DragList data={{
+          columns: [
+            {
+              id: 1,
+              label: '📫 Todo'
+            },
+            {
+              id: 2,
+              label: '✅ Done'
+            }
+          ],
+          cards: [
+            {
+              column: 1,
+              id: 'a',
+              title: 'Wash Dishes'
+            },
+            {
+              column: 2,
+              id: 'b',
+              title: 'Code DND Example'
+            }
+          ]
+        }} />
       </div>
     </div>
     {/if}
