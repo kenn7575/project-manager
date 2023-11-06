da0d9c862e99af09598314ff5fe6db4a7ea77a43
Kenni Kollemorten <kenn7575@gmail.com>
2023-11-06 17:12:30 +0100

    small UI ajustments and test user data

--- src/components/Sidebar.svelte
+++ src/components/Sidebar.svelte
@@ -1,5 +1,7 @@
 <script>
     import {LogOut} from '../functions/firebase'
+    import {user} from '../stores/userStore'
+    import {userData} from '../stores/userDataStore'
 
     const userProjects = [
         {
@@ -19,28 +21,33 @@
 </script>
 
 <div class={`fixed bg-base-200 h-screen w-[18rem]`}>
-    <div class="flex flex-col text-center h-full">
+    <div class="flex flex-col justify-between h-full px-3">
 
-        <div class="flex p-10 font-bold">
-            <h1 class="center m-auto">Sidebar</h1>
-        </div>
+        <div class="flex flex-col text-center h-full  gap-8 mt-8">
 
-        <div class="flex flex-col gap-4 items-center w-full px-3 overflow-y-auto">
-            {#each userProjects as project}
-                <div class="btn-ghost bg-neutral hover:bg-base-300 rounded-lg px-5 py-2 w-full text-left font-medium flex justify-between">
-                    <p>{project.name}</p>
-                    <div class="flex gap-6 items-center">
-                        <a href={`/project/${project.id}`}>
-                            <i class="fas fa-up-right-from-square hover:text-success project-button" title="Open project"/>
-                        </a>
-                        <i class="fas fa-pen hover:text-info project-button" title="Edit project"/>
-                        <i class="fas fa-trash-can hover:text-error project-button" title="Delete project" />
+            <div class="flex font-bold items-center justify-start gap-2">
+                <img class="w-8 h-8 rounded-full" src="{$user?.photoURL}" alt="profile">
+                <p> {$userData?.name}</p>
+               
+            </div>
+    
+            <div class="flex flex-col gap-4 items-center w-full  overflow-y-auto">
+                {#each userProjects as project}
+                    <div class="btn-ghost bg-neutral hover:bg-base-300 rounded-lg px-5 py-2 w-full text-left font-medium flex justify-between">
+                        <p>{project.name}</p>
+                        <div class="flex gap-6 items-center">
+                            <a href={`/project/${project.id}`}>
+                                <i class="fas fa-up-right-from-square hover:text-success project-button" title="Open project"/>
+                            </a>
+                            <i class="fas fa-pen hover:text-info project-button" title="Edit project"/>
+                            <i class="fas fa-trash-can hover:text-error project-button" title="Delete project" />
+                        </div>
                     </div>
-                </div>
-            {/each}
+                {/each}
+            </div>
         </div>
 
-        <div class="mt-auto p-10 font-bold">
+        <div class="font-bold mb-4">
             <button on:click={LogOut}>Sign out</button>
         </div>
     </div>
