ba2d81e675fb053f09eda0014bd81c2d8c0f2891
david <davidws13@outlook.com>
2023-11-09 10:17:59 +0100

    Tasksss

--- src/components/Task.svelte
+++ src/components/Task.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-  const priorityColors = ["success", "info", "warning", "error"];
   export let task: TaskType;
   import type { TaskType } from "../types/task";
 </script>
@@ -8,67 +7,18 @@
   {task.title}
   <div class="flex items-center justify-between gap-1">
     <p>Priority</p>
-    <div class="dropdown">
-      {#if task.priority == 0}
-        <label tabindex="0" class={`btn w-28 btn-sm btn-success m-1`}>Low</label
-        >
-      {:else if task.priority == 1}
-        <label tabindex="0" class={`btn w-28 btn-sm btn-warning m-1`}
-          >Medium</label
-        >
-      {:else if task.priority == 2}
-        <label
-          tabindex="0"
-          class="btn w-28 btn-sm btn-warning bg-orange-400 m-1 border-orange-400 hover:bg-orange-400 hover:border-orange-400"
-          >High</label
-        >
-      {:else if task.priority == 3}
-        <label tabindex="0" class="btn w-28 btn-sm btn-error m-1"
-          >Very High</label
-        >
-      {/if}
-      <!-- svelte-ignore a11y-no-noninteractive-tabindex -->
-      <ul
-        tabindex="0"
-        class="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-32"
+    {#if task.priority == 0}
+      <div class="badge badge-success gap-2">Low</div>
+    {:else if task.priority == 1}
+      <div class="badge badge-warning gap-2">Medium</div>
+    {:else if task.priority == 2}
+      <div
+        class="badge badge-warning bg-orange-400 m-1 border-orange-400 hover:bg-orange-400 hover:border-orange-400 gap-2"
       >
-        <li>
-          <button
-            class="text-success"
-            on:click={() => {
-              task.priority = 0;
-            }}
-            >Low
-          </button>
-        </li>
-        <li>
-          <button
-            class="text-warning"
-            on:click={() => {
-              task.priority = 1;
-            }}
-            >Medium
-          </button>
-        </li>
-        <li>
-          <button
-            class="text-orange-400"
-            on:click={() => {
-              task.priority = 2;
-            }}
-            >High
-          </button>
-        </li>
-        <li>
-          <button
-            class="text-error"
-            on:click={() => {
-              task.priority = 3;
-            }}
-            >Very High
-          </button>
-        </li>
-      </ul>
-    </div>
+        High
+      </div>
+    {:else if task.priority == 3}
+      <div class="badge badge-error gap-2">Very High</div>
+    {/if}
   </div>
 </div>
--- src/components/TaskDetailsModel.svelte
+++ src/components/TaskDetailsModel.svelte
@@ -7,12 +7,75 @@
 </script>
 
 <dialog id="my_modal_1" class="modal" bind:this={modal}>
-  <div class="modal-box">
+  <div class="modal-box w-4/5 h-3/6 p-16">
     <h3 class="font-bold text-lg">{task.title}</h3>
     <p>Edit task</p>
     <div class="modal-action w-full justify-center flex">
-      <!-- todo: add edit content here -->
-      <button class="btn btn-primary">edit something</button>
+      <div class="flex justify-between w-full">
+        <p>Priority</p>
+        <div class="dropdown">
+          {#if task.priority == 0}
+            <label tabindex="0" class={`btn w-28 btn-sm btn-success m-1`}
+              >Low</label
+            >
+          {:else if task.priority == 1}
+            <label tabindex="0" class={`btn w-28 btn-sm btn-warning m-1`}
+              >Medium</label
+            >
+          {:else if task.priority == 2}
+            <label
+              tabindex="0"
+              class="btn w-28 btn-sm btn-warning bg-orange-400 m-1 border-orange-400 hover:bg-orange-400 hover:border-orange-400"
+              >High</label
+            >
+          {:else if task.priority == 3}
+            <label tabindex="0" class="btn w-28 btn-sm btn-error m-1"
+              >Very High</label
+            >
+          {/if}
+          <!-- svelte-ignore a11y-no-noninteractive-tabindex -->
+          <ul
+            class="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-32"
+          >
+            <li>
+              <button
+                class="text-success"
+                on:click={() => {
+                  task.priority = 0;
+                }}
+                >Low
+              </button>
+            </li>
+            <li>
+              <button
+                class="text-warning"
+                on:click={() => {
+                  task.priority = 1;
+                }}
+                >Medium
+              </button>
+            </li>
+            <li>
+              <button
+                class="text-orange-400"
+                on:click={() => {
+                  task.priority = 2;
+                }}
+                >High
+              </button>
+            </li>
+            <li>
+              <button
+                class="text-error"
+                on:click={() => {
+                  task.priority = 3;
+                }}
+                >Very High
+              </button>
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
   </div>
   <form method="dialog" class="modal-backdrop">
