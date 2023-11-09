068798766b047e46c3b6edfc8c990c0060b087d9
Kenni Kollemorten <kenn7575@gmail.com>
2023-11-09 14:13:56 +0100

    fix

--- src/stores/userStore.ts
+++ src/stores/userStore.ts
@@ -5,25 +5,13 @@ import { isDoneLoadingStore } from "../stores/loadingStore";
 import { getDoc, doc, setDoc } from "firebase/firestore";
 import { db } from "../functions/firebase";
 
-async function userStore() {
+function userStore() {
   let unsubscribe: () => void;
 
   const { subscribe } = writable(auth?.currentUser ?? null, (set) => {
     unsubscribe = onAuthStateChanged(auth, async (user) => {
       //check if user doc exists
-      const docRef = doc(db, "users/" + auth.currentUser?.uid);
-      const docSnap = await getDoc(docRef);
-      if (!docSnap.exists()) {
-        //create user document
-        const newUser = {
-          uid: user?.uid,
-          name: user?.displayName,
-          email: user?.email,
-          photoURL: user?.photoURL,
-          dateCreated: new Date(),
-        };
-        await setDoc(docRef, newUser);
-      }
+
       isDoneLoadingStore.set(true);
       set(user);
     });
