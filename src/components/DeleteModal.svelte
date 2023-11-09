2ce851449d463d851098f131d8e6fb5d43b99c57
Nima - HF2 <nima0100@edu.sde.dk>
2023-11-09 12:38:31 +0100

    no muchacho

--- src/components/DeleteModal.svelte
+++ src/components/DeleteModal.svelte
@@ -8,11 +8,11 @@
 <dialog id="editModal" class="modal" bind:this={modal}>
   <div class="modal-box flex flex-col gap-5">
     <h3 class="font-bold text-lg">{project.title}</h3>
-    <p>Er du sikker på at du vil slette {project.title}?</p>
+    <p>Er du sikker på at du vil slette <b class="font-extrabold">{project.title}</b>?</p>
     <div class="modal-action">
       <form method="dialog" class="flex gap-5">
         <!-- if there is a button in form, it will close the modal -->
-        <button class="btn">Close</button>
+        <button class="btn">Cancel</button>
         <button on:click={() => deleteDocument(`projects/${project.id}`)} class="btn btn-error">Delete</button>
       </form>
     </div>
