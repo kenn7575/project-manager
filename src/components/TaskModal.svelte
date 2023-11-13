f863899119244c0d8d59d66c1fff8370da6b40fb
david <davidws13@outlook.com>
2023-11-13 08:29:43 +0100

    Fjernet bg-neutral

--- src/components/TaskModal.svelte
+++ src/components/TaskModal.svelte
@@ -1,46 +1,42 @@
 <script lang="ts">
-    import TaskDetailsModel from "./TaskDetailsModel.svelte";
-    import Task from "./Task.svelte";
+  import TaskDetailsModel from "./TaskDetailsModel.svelte";
+  import Task from "./Task.svelte";
 
-    import { draggable } from "../functions/dragAndDrop"
-    import type { TaskType } from "../types/task"
-    import type { ProjectType } from "../types/project"
-    import { updateDocument } from "../functions/firebase";
+  import { draggable } from "../functions/dragAndDrop";
+  import type { TaskType } from "../types/task";
+  import type { ProjectType } from "../types/project";
+  import { updateDocument } from "../functions/firebase";
 
-    let editModal
-    const priorityColors = ["success", "info", "warning", "error"];
+  let editModal;
+  const priorityColors = ["success", "info", "warning", "error"];
 
-    export let task: TaskType;
-    export let project: ProjectType;
-    let shadowProject = { ...project };
+  export let task: TaskType;
+  export let project: ProjectType;
+  let shadowProject = { ...project };
 
-    function update(task) {
-        const index = shadowProject.tasks.findIndex(t => t.id == task.id)
-        shadowProject.tasks[index] = task;
-        updateDocument("projects/" + project.id, shadowProject)
-    }
-
+  function update(task) {
+    const index = shadowProject.tasks.findIndex((t) => t.id == task.id);
+    shadowProject.tasks[index] = task;
+    updateDocument("projects/" + project.id, shadowProject);
+  }
 </script>
 
 <TaskDetailsModel
-    bind:modal={editModal}
-    bind:task
-    on:update={() => {
+  bind:modal={editModal}
+  bind:task
+  on:update={() => {
     update(task);
-    editModal.close()
-    }}
+    editModal.close();
+  }}
 />
 
 <li
-    class={` bg-neutral relative rounded-lg border hover:bg-slate-600 border-neutral-300 bg-[${
+  class={` bg-neutral relative rounded-lg border hover:bg-slate-600 border-neutral-300 bg-[${
     priorityColors[task.priority]
-    }]`}
-    use:draggable={task.id}
+  }]`}
+  use:draggable={task.id}
 >
-    <button
-    class="p-4 w-full"
-    on:click={() => editModal.showModal()}
-    >
+  <button class="p-4 w-full" on:click={() => editModal.showModal()}>
     <Task bind:task />
-    </button>
+  </button>
 </li>
